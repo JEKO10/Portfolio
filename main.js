3c5ae53a28d5165d27b9a3620690193298c1f31d
Aleksa Bubanja <aleksa3b@gmail.com>
2022-12-15 23:04:11 +0100

    nav list design

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const menuBtn = document.querySelector(".menuBtn");
 const menuBurger = document.querySelector(".menuBurger");
 const navList = document.querySelector("nav ul");
+const navListItems = document.querySelectorAll("nav ul li");
 const socials = document.querySelector(".hero ul");
 
 menuBtn.addEventListener("click", () => {
   menuBurger.classList.toggle("open");
   navList.classList.toggle("hide");
   socials.classList.toggle("hide");
+  document.documentElement.classList.toggle("hide");
+});
+
+navListItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    menuBurger.classList.remove("open");
+    navList.classList.add("hide");
+    socials.classList.add("hide");
+    document.documentElement.classList.remove("hide");
+  });
 });
