72c497b7e1e222494ecff2b94fbd06167bd22f28
Aleksa Bubanja <aleksa3b@gmail.com>
2023-01-05 20:22:54 +0100

    contact alerts

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const navList = document.querySelector("nav ul");
 const navListItems = document.querySelectorAll("nav ul li");
 const socials = document.querySelector(".hero ul");
 const submit = document.querySelector("#submit");
+let senderName = document.querySelector("#name");
+let email = document.querySelector("#email");
+let message = document.querySelector("#message");
+const alertMessage = document.querySelector("form p");
 
 menuBtn.addEventListener("click", () => {
   menuBurger.classList.toggle("open");
@@ -34,14 +38,24 @@ window.addEventListener("resize", () => {
 submit.addEventListener("click", (e) => {
   e.preventDefault();
   const params = {
-    from_name: document.querySelector("#name").value,
-    email_id: document.querySelector("#email").value,
-    message: document.querySelector("#message").value,
+    from_name: senderName.value,
+    email_id: email.value,
+    message: message.value,
   };
 
-  emailjs
-    .send("service_h26arzu", "template_8mah7dd", params)
-    .then(function (res) {
-      alert("Status " + res.status);
-    });
+  if (senderName.value && email.value && message.value) {
+    emailjs
+      .send("service_h26arzu", "template_8mah7dd", params)
+      .then(function () {
+        message.value = "";
+        alertMessage.textContent = "Your message has been sent successfully!";
+        alertMessage.style.display = "block";
+        setTimeout(() => {
+          alertMessage.style.display = "none";
+        }, 5000);
+      });
+  } else {
+    alertMessage.textContent = "All fields are required!";
+    alertMessage.style.display = "block";
+  }
 });
