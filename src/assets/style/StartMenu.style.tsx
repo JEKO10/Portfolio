621849b95fbe1fafb21b41edd286de9eb719ecc6
Aleksa Bubanja <aleksa3b@gmail.com>
2023-10-21 23:51:56 +0200

    start menu redesign

--- src/assets/style/StartMenu.style.tsx
+++ src/assets/style/StartMenu.style.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import start from "../images/modals/startMenu.jpg";
+import start from "../images/modals/startMenuBg.jpg";
 
 type StartContainerPosition = {
   isOpen: boolean | undefined;
@@ -10,25 +10,37 @@ export const StartContainer = styled.section<StartContainerPosition>`
   background: url(${start}) center/contain no-repeat;
   height: 475px;
   width: 337px;
+  padding: 6px 6.21px 8px 3.5px;
   transform: ${({ isOpen }) => (isOpen ? "translateY(0)" : "translateY(110%)")};
   transition: transform 500ms ease;
   position: absolute;
-  bottom: 3.4rem;
+  bottom: 2.81rem;
   z-index: 10;
-  pointer-events: none;
-  user-select: none;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
 
-  img {
+  article {
     height: 91px;
-    width: 337px;
+    width: 100%;
+    padding: 20px 25px;
     cursor: pointer;
-    /* position: absolute;
-    left: 0;
-    margin-left: 0.1rem; */
 
-    &:first-of-type {
-      height: 104px;
+    & {
+      :hover {
+        background-color: #001358;
+      }
+
+      :last-of-type {
+        padding: 20px 25px 8px;
+      }
     }
+  }
+
+  > img {
+    display: block;
+    margin: 0.5rem auto 0;
   }
 `;
 
@@ -38,3 +50,16 @@ export const StartDash = styled.img`
   display: block;
   margin: 0 auto;
 `;
+
+// img {
+//     height: 91px;
+//     width: 337px;
+//     cursor: pointer;
+//     /* position: absolute;
+//     left: 0;
+//     margin-left: 0.1rem; */
+
+//     &:first-of-type {
+//       height: 104px;
+//     }
+//   }
--- src/components/windows/StartMenu.tsx
+++ src/components/windows/StartMenu.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import About from "../../assets/images/buttons/aboutStart.jpg";
-import Contact from "../../assets/images/buttons/contactStart.jpg";
-import Recycle from "../../assets/images/buttons/recycleStart.jpg";
-import ShutDown from "../../assets/images/buttons/shutDown.jpg";
-import Work from "../../assets/images/buttons/workStart.jpg";
+import About from "../../assets/images/buttons/aboutStart.png";
+import Contact from "../../assets/images/buttons/contactStart.png";
+import Recycle from "../../assets/images/buttons/recycleStart.png";
+import ShutDown from "../../assets/images/buttons/shutDown.png";
+import Work from "../../assets/images/buttons/workStart.png";
 import Dash from "../../assets/images/other/startDash.jpg";
-import { StartContainer, StartDash } from "../../assets/style/StartMenu.style";
+import { StartContainer } from "../../assets/style/StartMenu.style";
 import { useGlobalContext } from "../../context";
 
 const StartMenu: React.FC = () => {
@@ -15,7 +15,44 @@ const StartMenu: React.FC = () => {
 
   return (
     <StartContainer isOpen={isOpen.start}>
-      <img
+      <article
+        onClick={() => {
+          setIsOpen({ ...isOpen, about: true, start: false });
+          setIsVisible({ ...isVisible, about: true });
+        }}
+      >
+        <img src={About} alt="AboutStart" />
+      </article>
+      <article
+        onClick={() => {
+          setIsOpen({ ...isOpen, work: true, start: false });
+          setIsVisible({ ...isVisible, work: true });
+        }}
+      >
+        <img src={Work} alt="WorkStart" />
+      </article>
+      <article
+        onClick={() => {
+          setIsOpen({ ...isOpen, contact: true, start: false });
+          setIsVisible({ ...isVisible, contact: true });
+        }}
+      >
+        <img src={Contact} alt="ContactStart" />
+      </article>
+      <article
+        onClick={() => {
+          setIsOpen({ ...isOpen, recycle: true, start: false });
+          setIsVisible({ ...isVisible, recycle: true });
+        }}
+      >
+        <img src={Recycle} alt="RecycleStart" />
+      </article>
+      <img src={Dash} alt="StartDash" />
+      <article onClick={() => setIsShutDown(true)}>
+        <img src={ShutDown} alt="ShutDownStart" />
+      </article>
+
+      {/* <img
         src={About}
         alt="About"
         onClick={() => {
@@ -48,7 +85,7 @@ const StartMenu: React.FC = () => {
         }}
       />
       <StartDash src={Dash} alt="StartDash" />
-      <img src={ShutDown} alt="ShutDown" onClick={() => setIsShutDown(true)} />
+      <img src={ShutDown} alt="ShutDown" onClick={() => setIsShutDown(true)} /> */}
     </StartContainer>
   );
 };
