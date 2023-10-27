badd90428e8468090efc95fa3d2389af297590f0
Aleksa Bubanja <aleksa3b@gmail.com>
2023-10-27 20:12:39 +0200

    book file setup

--- src/components/windows/Book.tsx
+++ src/components/windows/Book.tsx
@@ -1,17 +1,44 @@
 import React from "react";
+import Draggable from "react-draggable";
 
 import { BookFile } from "../../assets/style/Book.style";
+import { CloseBtn, MinimizeBtn } from "../../assets/style/Files.style";
 import { useGlobalContext } from "../../context";
 
 const Book = () => {
-  const { lastClicked, isVisible, setLastClicked } = useGlobalContext();
+  const {
+    isOpen,
+    setIsOpen,
+    lastClicked,
+    isVisible,
+    setLastClicked,
+    setIsVisible,
+  } = useGlobalContext();
 
   return (
-    <BookFile
-      onMouseDownCapture={() => setLastClicked("book")}
-      lastClicked={lastClicked}
-      isVisible={isVisible.book}
-    ></BookFile>
+    <Draggable defaultPosition={{ x: 100, y: -400 }} bounds="body">
+      <BookFile
+        onMouseDownCapture={() => setLastClicked("book")}
+        lastClicked={lastClicked}
+        isVisible={isVisible.book}
+        className="file"
+      >
+        <MinimizeBtn
+          onMouseUp={() => setIsVisible({ ...isVisible, book: false })}
+          height={28}
+          width={32}
+          top={0.75}
+          right={5.1}
+        />
+        <CloseBtn
+          onMouseUp={() => setIsOpen({ ...isOpen, book: false })}
+          height={32}
+          width={32.5}
+          top={0.6}
+          right={0.75}
+        />
+      </BookFile>
+    </Draggable>
   );
 };
 
