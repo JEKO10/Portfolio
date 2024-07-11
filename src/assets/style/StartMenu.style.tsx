6314c2eba7de186dca13ccdf1e38b4d772f89d6e
Aleksa <aleksa3b@gmail.com>
2024-07-11 22:09:57 +0200

    start menu responsive

--- src/assets/style/StartMenu.style.tsx
+++ src/assets/style/StartMenu.style.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 import start from "../images/modals/startMenuBg.jpg";
-import { disableUserShared, flexMixin } from "./GlobalStyles";
+import { desktopL, disableUserShared, flexMixin } from "./GlobalStyles";
 
 type StartContainerPosition = {
   isOpen: boolean | undefined;
@@ -16,16 +16,22 @@ export const StartContainer = styled.section<StartContainerPosition>`
   position: absolute;
   bottom: 2.81rem;
   padding: 6px 6.21px 8px 3.5px;
-  transform: ${({ isOpen }) => (isOpen ? "translateY(0)" : "translateY(110%)")};
+  transform: ${({ isOpen }) => (isOpen ? "translateY(0)" : "translateY(0%)")};
   transition: transform 500ms ease;
   z-index: 10;
 
-  /* height: 300px;
-  width: 212px; */
+  @media ${desktopL} {
+    height: 300px;
+    width: 212px;
+  }
 
   > img {
     display: block;
     margin: 0 auto;
+
+    @media ${desktopL} {
+      width: 204.5px;
+    }
   }
 
   img,
@@ -41,6 +47,11 @@ export const StartMenuBtn = styled.article`
   padding: 20px 25px;
   transition: all 200ms ease;
 
+  @media ${desktopL} {
+    height: 55px;
+    padding: 10px 15px;
+  }
+
   & {
     :hover {
       background-color: #001358;
@@ -49,6 +60,10 @@ export const StartMenuBtn = styled.article`
 
     :last-of-type {
       padding: 20px 25px 8px;
+
+      @media ${desktopL} {
+        padding: 10px 15px 5px;
+      }
     }
   }
 
@@ -58,9 +73,19 @@ export const StartMenuBtn = styled.article`
 
     margin-right: 1.8rem;
     image-rendering: pixelated;
+
+    @media ${desktopL} {
+      height: 39px;
+      width: 39px;
+      margin-right: 1rem;
+    }
   }
 
   p {
     font-size: 1.8rem;
+
+    @media ${desktopL} {
+      font-size: 1.4rem;
+    }
   }
 `;
